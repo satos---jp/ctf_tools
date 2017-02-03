2cfe3893e4c807ee10df082d700a1ca93e36895a
satos---jp <syogo8118@yahoo.co.jp>
2017-02-03 06:54:12 -0800

    fix shellcode

--- x86-execve.s
+++ x86-execve.s
@@ -1,17 +1,22 @@
 BITS 32
-jmp short sec
-fir:
 xor edx, edx
-pop ebx
 push edx
+jmp short arg0
+fir:
+pop ebx
 push ebx
 mov ecx, esp
 xor eax, eax
 mov al, 11
 int 0x80
-sec:
+;arg1:
+;call near arg0
+;db "arg1"
+;db 0
+arg0:
 call near fir
 db "/bin/sh"
+db 0
 
 
 ; nasm tes.s 
@@ -20,4 +25,14 @@ db "/bin/sh"
 ; char s[] = "/bin/sh";
 ; char* ss[2] = {s,0};
 ;	execve(s,ss,0);
+
+;BITS 32
+;global _start
+;_start:
+;	mov eax,1
+;	int 0x80
+
+; nasm -f elf32 tes.s
+; gcc -nostdlib -m32 tes.o
+
 
